d8efb134c3fe7686e2dd70053edbcfbf447668da
Rajinder Singh <rajindersingh@hotmail.com>
2014-02-07 19:10:03 -0600

    updated settings

--- hdpsetup.sh
+++ hdpsetup.sh
@@ -1,12 +1,12 @@
 #This settings file stores all the settings related to HDP cluster you are setting up
 #Affinty group helps you keep your storage and compute in the same region
-export affinityGroupName=rajeasthdpag
+export affinityGroupName=youreasthdpag
 #Name the region where affinity group should be created. 
 #choices are valid values are "East US", "West US", "East Asia", "Southeast Asia", "North Europe", "West Europe"
 export affinityGroupLocation="East US"
 
 #name of the storage account here your virtual machines will be stored.
-export storageAccount=rajhdpstorage
+export storageAccount=yourhdpstorage
 
 #Name of the image you will use to create your virtual machines
 #export imageName=b39f27a8b8c64d52b05eac6a62ebad85__Ubuntu_DAILY_BUILD-precise-12_04_3-LTS-amd64-server-20140204-en-us-30GB
@@ -21,14 +21,14 @@ export diskSizeInGB=5
 export numOfDisks=1
 
 #virtual machine settings
-export vmNamePrefix=rajbdhdp
-export cloudServicePrefix=rajbdhdp
+export vmNamePrefix=yourbdhdp
+export cloudServicePrefix=yourbdhdp
 #user name and password for the virtual machine you are creating
 export adminUserName=azureuser
 export adminPassword=Password.1!
 
 #setting related to virtual network
-export vnetName=rajeasthdp
+export vnetName=youreasthdpvnet
 #address space allows 192.168.0.0, 10.0.0.0 and 172.16.0.0 ip address ranges
 #virtual network faq is here http://msdn.microsoft.com/en-us/library/windowsazure/dn133803.aspx
 export vnetAddressSpace=172.16.0.0
@@ -39,6 +39,9 @@ export subnetCidr=24
 
 #These settings are for nodes in the HDP cluster
 #Name of the custom image you will use to create your cluster nodes
-export nodeImageName=b39f27a8b8c64d52b05eac6a62ebad85__Ubuntu_DAILY_BUILD-precise-12_04_3-LTS-amd64-server-20140204-en-us-30GB
+#After you have create your master node image replace the value of nodeImageName wiht the image you created 
+export nodeImageName=c290a6b031d841e09f2da759bbabe71f__Oracle-Linux-6
+#Number of nodes in your HDP cluster
 export nodeCount=2
+#Size of the nodes in the hadoop cluster. Valid sizes are extrasmall, small, medium, large, extralarge, a5, a6, a7
 export nodeSize=small
